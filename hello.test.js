e7c01edef896c38fb5c15a13888754bec2825cae
Matteo Presot <matteo.presot@gmail.com>
2023-11-04 18:40:13 +0100

    Improve the Sum Function

--- hello.test.js
+++ hello.test.js
@@ -2,5 +2,24 @@ import { test, expect } from 'vitest'
 
 test('1 + 1', () => {
 
-  expect(1+1).toEqual(2)
+  expect(sum(1,1)).toEqual(2)
 })
+
+test('1 + 2 + 3 = 6', () => {
+
+  expect(sum(1,2,3)).toEqual(6)
+})
+
+test('1 + 2 + 3 + 4 ... = 6', () => {
+
+  expect(sum(1,2,3, 4)).toEqual(10)
+})
+
+test('0', () => {
+
+  expect(sum()).toEqual(0)
+})
+
+function sum(...nums) {
+  return nums.reduce((res, value) => res + value, 0)
+}
